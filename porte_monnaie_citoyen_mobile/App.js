8ff91ff17c514af3ab959298c7ff56addf9337e6
Bastien Pierre <bastien.pierre@outlook.com>
2018-09-24 00:26:41 +0200

    merge all

--- porte_monnaie_citoyen_mobile/App.js
+++ porte_monnaie_citoyen_mobile/App.js
@@ -1,12 +1,10 @@
 import HomeScreen from './views/Home';
 import {createStackNavigator} from 'react-navigation';
 import ProfileScreen from './views/Profile';
-import ConnectionScreen from './views/Connection';
-import MissionScreen from './views/Mission'
+import MissionScreen from './views/Mission';
 
 const App = createStackNavigator({
-  Connection: {screen: ConnectionScreen, navigationOptions:{header: null}},
-  Home: {screen: HomeScreen},
+  Home: {screen: HomeScreen, navigationOptions:{header: null}},
   Profile: {screen: ProfileScreen},
   Mission: {screen: MissionScreen}
 });
