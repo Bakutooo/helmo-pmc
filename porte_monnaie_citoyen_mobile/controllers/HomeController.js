6d94b328952118d125bfeeba41dd5c7df7e051a8
Bastien Pierre <bastien.pierre@outlook.com>
2018-09-23 20:02:54 +0200

    Ajout server info

---
+++ porte_monnaie_citoyen_mobile/controllers/HomeController.js
@@ -0,0 +1,13 @@
+export default class HomeController {
+    constructor(view){
+        this.view = view;
+    }
+
+    getAllMissions(){
+        return new Promise((resolve, reject) => {
+            fetch()
+        });
+    }
+
+
+}
