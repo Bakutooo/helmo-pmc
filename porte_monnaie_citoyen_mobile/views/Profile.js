367c14d4d33241fe91b03822b739e1de1ff97624
Léonard <l.leonard@student.helmo.be>
2018-09-24 01:44:41 +0200

    profile css done

--- porte_monnaie_citoyen_mobile/views/Profile.js
+++ porte_monnaie_citoyen_mobile/views/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, FlatList, AsyncStorage} from 'react-native';
+import {View, Text, FlatList, AsyncStorage, Image, ScrollView} from 'react-native';
 import style from './../style';
 import ProfileController from './../controllers/ProfileController';
 
@@ -37,14 +37,30 @@ export default class Profile extends React.Component {
     
     render(){
         return (
-            <View style={style.profile}>
-                <Text style={{fontSize: 40}}>{this.state.user.firstname} {this.state.user.name.toUpperCase()}</Text>
-                <Text style={{fontSize: 20}}>Solde : {this.state.user.sold} PC</Text>
-                <FlatList
-                    data={this.state.user.missions}
-                    renderItem={({item}) => <Text style={style.content_row}>{item.name}</Text>}
-                />
-            </View>
+            <ScrollView>
+                <View style={{padding : 20}}>
+                    <Image
+                        source={{uri: 'https://via.placeholder.com/150x150'}}
+                        style={{width : 150, height : 150, marginTop : 40, alignSelf : 'center'}}
+                    /> 
+
+                    <Text style={{fontSize: 17, marginTop : 30, fontWeight : 'bold'}}>Mail :</Text>
+                    <Text style={{fontSize: 17, marginBottom : 20}}>{this.state.user.mail}</Text>
+
+                    <Text style={{fontSize: 17, fontWeight : 'bold'}}>Nom de famille :</Text>
+                    <Text style={{fontSize: 17, marginBottom : 20}}>{this.state.user.name}</Text>
+
+                    <Text style={{fontSize: 17, fontWeight : 'bold'}}>Prénom :</Text>
+                    <Text style={{fontSize: 17, marginBottom : 20}}>{this.state.user.firstname}</Text>
+
+                    <Text style={{fontSize: 17, textAlign : 'center'}}>Solde : {this.state.user.sold} points citoyen</Text>
+
+                    <FlatList
+                        data={this.state.user.missions}
+                        renderItem={({item}) => <Text style={style.content_row}>{item.name}</Text>}
+                    />
+                </View>
+            </ScrollView>
         );
     }
 }
