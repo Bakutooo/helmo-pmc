d110937a766dceb0460ea15c04fb91f1ef3125e1
Bastien Pierre <bastien.pierre@outlook.com>
2018-09-23 17:51:48 +0200

    Home et Menu et Profile Screens

--- porte_monnaie_citoyen_mobile/App.js
+++ porte_monnaie_citoyen_mobile/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import HomeScreen from './views/Home';
+import {createStackNavigator} from 'react-navigation';
+import ProfileScreen from './views/Profile';
 
-export default class App extends React.Component {
-  render(){
-    return (
-      <View>
-        <Text>Hello World!</Text>
-      </View>
-    );
-  }
-}
+const App = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen}
+});
+
+export default App;
---
+++ porte_monnaie_citoyen_mobile/style.js
@@ -0,0 +1,35 @@
+export default {
+    button_menu: {
+        fontSize: 30,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    menu: {
+        width: "45%",
+        height: "100%",
+        backgroundColor: 'white'
+    },
+    row: {
+        margin: 5,
+        padding: 5,
+        backgroundColor: "#BBBBBB"
+    },
+    title_row: {
+        fontSize: 30
+    },
+    content_row: {
+        fontSize: 20
+    },
+    profile: {
+        height: "100%"
+    },
+    button_search: {
+        position: "absolute",
+        bottom: 5,
+        right: 5,
+        width: 20,
+        height: 20,
+        backgroundColor: "#BBBBBB"
+    }
+}
