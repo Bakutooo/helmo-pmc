26d1beef77e8093ed49f4098dfa5ab8248c57894
Bastien Pierre <bastien.pierre@outlook.com>
2018-09-23 18:44:20 +0200

    correction serveur

--- porte_monnaie_citoyen_server/routes/transaction.js
+++ porte_monnaie_citoyen_server/routes/transaction.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Transaction = require('./../models/Transaction');
+const Citizen = require('./../models/Citizen');
 
 
 router.get('/', (req, res) => {
     Transaction.find()
+        .populate('sender')
+        .populate('receiver')
         .then(transaction => res.json(transaction));
 });
 
 
 router.post('/', (req, res) => {
+    let citizen = req.body.sender;
     let newTransaction = new Transaction({
-        sender: req.body.sender,
+        sender: citizen,
         receiver: req.body.receiver,
         amount: req.body.amount,
         date: Date.now()
     });
 
     newTransaction.save()
-        .then(res.json(newTransaction));
+        .then(() => {
+            Citizen.findOne({_id: citizen._id}, (err, doc) => {
+                doc.sold = (doc.sold - newTransaction.amount);
+                doc.save();
+            });
+            res.json(newTransaction);
+        });
 });
 
 module.exports = router;
