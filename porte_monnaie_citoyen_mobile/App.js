0e97c17a5113385e699c36b429f8ca7dca991bbb
Bastien Pierre <bastien.pierre@outlook.com>
2018-09-23 15:11:58 +0200

    correction mobile

--- porte_monnaie_citoyen_mobile/App.js
+++ porte_monnaie_citoyen_mobile/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {View, Text} from 'react-native';
+
 export default class App extends React.Component {
   render(){
     return (
       <View>
-        <Text>Hello world</Text>
+        <Text>Hello World!</Text>
       </View>
     );
   }
