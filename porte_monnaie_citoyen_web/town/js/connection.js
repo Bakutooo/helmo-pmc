01a19e0dd01ec31f734e31b06dbbd52a640ec8b5
Arnaud RUELL <a.ruell@student.helmo.be>
2018-09-24 21:37:49 +0200

    b4 merge with phil

---
+++ porte_monnaie_citoyen_web/town/js/connection.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("input[name='button']").on('click', (evt) => {
+        evt.preventDefault();
+        let name = $("input[name='name']").val();
+        let password = $("input[name='password'").val();
+
+        fetch
+    })
+});
