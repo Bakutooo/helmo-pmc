56c58edca603e618aa817430c8c50329fc5a682b
Arnaud RUELL <a.ruell@student.helmo.be>
2018-09-24 05:03:46 +0200

    partners v1

--- porte_monnaie_citoyen_mobile/App.js
+++ porte_monnaie_citoyen_mobile/App.js
@@ -2,11 +2,13 @@ import HomeScreen from './views/Home';
 import {createStackNavigator} from 'react-navigation';
 import ProfileScreen from './views/Profile';
 import MissionScreen from './views/Mission';
+import PartnersScreen from './views/Partners'
 
 const App = createStackNavigator({
   Home: {screen: HomeScreen, navigationOptions:{header: null}},
   Profile: {screen: ProfileScreen},
-  Event: {screen: MissionScreen}
+  Event: {screen: MissionScreen},
+  Partners: {screen: PartnersScreen}
 });
 
 export default App;
