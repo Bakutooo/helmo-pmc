3a0d582e741ebcc2ad1f6010ab0e4a386c4c8045
Bastien Pierre <bastien.pierre@outlook.com>
2018-09-24 21:08:42 +0200

    debut client partner

---
+++ porte_monnaie_citoyen_web/partner/js/connection.js
@@ -0,0 +1,27 @@
+$(function() {
+    $("input[name='button_co']").on('click', (evt) => {
+        evt.preventDefault();
+        let email = $("input[name='email']").val();
+        let password = $("input[name='password']").val();
+
+        fetch('http://localhost/backend/partner/connection', {
+            method: "POST",
+            body: JSON.stringify({mail: email, password: password}),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            }
+        })
+        .then(res => res.json())
+        .then(res => {
+            if(res.error === undefined){
+                $.get('partner/html/dashboard.html', (data) => {
+                    $('main').empty();
+                    $('main').append($(data));
+                    localStorage.setItem('partner', res);
+                });
+            } else {
+                $('#error').append(res.error);
+            }
+        });
+    })
+});
