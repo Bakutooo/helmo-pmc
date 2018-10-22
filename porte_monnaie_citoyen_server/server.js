2481f44058cc7101bd6044f1556c2269924ee197
Bastien PIERRE <b.pierre@student.helmo.be>
2018-10-22 08:39:37 +0200

    Serveur fini

--- porte_monnaie_citoyen_server/server.js
+++ porte_monnaie_citoyen_server/server.js
@@ -4,13 +4,11 @@ let bodyParser = require("body-parser");
 let db = require('./bd-info');
 
 let citizen = require('./routes/citizen');
-let mission = require('./routes/mission'); //non utiliser
 let town = require('./routes/town');
 let payment = require('./routes/payment');
 let partner = require('./routes/partner');
 let deal = require('./routes/deal');
 let event = require('./routes/event');
-let request = require('./routes/request');
 
 let app = express();
 
@@ -24,13 +22,11 @@ mongoose.connect(db.connection_string, { useNewUrlParser: true})
 
 //Routes
 app.use('/citizen', citizen);
-app.use('/mission', mission);
 app.use('/town', town);
 app.use('/payment', payment);
 app.use('/partner', partner);
 app.use('/deal', deal);
 app.use('/event', event);
-app.use('/request', request);
 
 //Server
 app.listen(30000, () => console.log('Server started on port 30000'));
