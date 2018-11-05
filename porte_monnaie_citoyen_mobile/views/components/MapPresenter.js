4483c266c15828923e30fbec25442d2f9f97569f
Bastien PIERRE <b.pierre@student.helmo.be>
2018-11-05 10:56:11 +0100

    App mobile done

--- porte_monnaie_citoyen_mobile/views/components/MapPresenter.js
+++ porte_monnaie_citoyen_mobile/views/components/MapPresenter.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import Geocoder from "react-native-geocoding";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export default class MapPresenter extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            region: {
+                latitude: 0,
+                longitude: 0,
+                longitudeDelta: 0,
+                latitudeDelta: 0
+            }
+        }
+    }
+
+    componentWillMount(){
         Geocoder.init('AIzaSyDq8hCZEHc0kPCl_1qfGpbP6lCyXy4tDnY');
-        Geocoder.from(`${this.props.address}`)
-                .then(res => console.log(res));
+        Geocoder.from(this.props.address)
+                .then(res => {
+                    let geo = res.results[0].geometry;
+                    this.setState({
+                        region: {
+                            latitude: Number(geo.location.lat), 
+                            longitude: Number(geo.location.lng),
+                            longitudeDelta: Number(geo.viewport.northeast.lng) - Number(geo.viewport.southwest.lng),
+                            latitudeDelta: Number(geo.viewport.northeast.lat) - Number(geo.viewport.southwest.lat)
+                        }
+                    });
+                    console.log(this.state.region);
+                })
+                .catch(err => console.log(err));
+        
     }
 
     render() {
         return (
-        <View>
-            <Text> Maps lieu: {this.props.address} </Text>
-        </View>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={{width: "100%", height: "100%"}}
+                initialRegion={this.state.region}
+            >
+                <Marker coordinate={this.state.region}/>
+            </MapView>
         );
     }
 }
