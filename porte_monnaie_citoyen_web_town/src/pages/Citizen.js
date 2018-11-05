e33a864c9da2027b59b29639f34d65fecd1b5b39
Bastien PIERRE <b.pierre@student.helmo.be>
2018-11-05 11:03:48 +0100

    Citizen added

---
+++ porte_monnaie_citoyen_web_town/src/pages/Citizen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Citizen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+        <div>
+            <header>
+                <h2>Citoyens :</h2>
+            </header>
+            <div>
+                
+            </div>
+        </div>
+    );
+  }
+}
