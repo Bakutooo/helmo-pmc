ebbfa47550b09916eda64371314fd9bbc4959913
Bastien PIERRE <b.pierre@student.helmo.be>
2018-11-06 19:46:40 +0100

    Navbar link

--- porte_monnaie_citoyen_web_town/src/pages/components/Navbar.js
+++ porte_monnaie_citoyen_web_town/src/pages/components/Navbar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
     render() {
         return (
             <div className="d-flex flex-column pmc-navbar">
-                <div><p>Accueil</p></div>                
-                <div><p>Évènement</p></div>
-                <div><p>Partenaire</p></div>
-                <div><p>Citoyen</p></div>
-                <div><p>Se déconnecter</p></div>
+                <Link to="/dashboard"><div>Accueil</div></Link>                
+                <Link to="/dashboard"><div>Évènement</div></Link>
+                <Link to="/dashboard"><div>Partenaire</div></Link>
+                <Link to="/citizen"><div>Citoyen</div></Link>
+                <Link to="/dashboard"><div>Se déconnecter</div></Link>
             </div>
         )
     }
