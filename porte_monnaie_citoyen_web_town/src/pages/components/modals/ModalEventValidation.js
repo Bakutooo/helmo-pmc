7c7368523a3731fb7a6ddb9d018cc215f61c77b7
Arnaud RUELL <a.ruell@student.helmo.be>
2018-11-08 21:07:49 +0100

    events validation and modification ui done

--- porte_monnaie_citoyen_web_town/src/pages/components/modals/ModalEventValidation.js
+++ porte_monnaie_citoyen_web_town/src/pages/components/modals/ModalEventValidation.js
@@ -68,8 +68,8 @@ export default class ModalEventValidation extends Component {
                                 </div>
                             </div>
                             <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Save changes</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                                <button type="button" className="btn btn-primary">Envoyer</button>
                             </div>
                         </div>
                     </div>
