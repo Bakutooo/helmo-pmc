4d347f88284f20058a1eb9538d7392f4c962b58a
Arnaud RUELL <a.ruell@student.helmo.be>
2018-12-03 09:01:42 +0100

    started dashboard

---
+++ porte_monnaie_citoyen_web_partner/src/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div className="row w-100">
+        <div className="w-75 p-3 event-demande">
+            <h4>Dernières demandes d'évènement :</h4>
+            {this.renderEvents()}
+        </div>
+
+        <div className="w-25 p-3">
+            <h4>Dernières demandes de partenariat :</h4>
+            {this.renderPartners()}
+        </div>
+    </div>
+</template>
+
+<script>
+    
+</script>
