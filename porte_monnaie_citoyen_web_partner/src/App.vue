e22d66c788c907fc5bbe405d6a5ab0da6da9c8b5
Arnaud RUELL <a.ruell@student.helmo.be>
2018-12-03 08:48:57 +0100

    signup view

--- porte_monnaie_citoyen_web_partner/src/App.vue
+++ porte_monnaie_citoyen_web_partner/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" data-toggle="modal" data-target="#signup">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
---
+++ porte_monnaie_citoyen_web_partner/src/components/modals/ModalSignup.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="modal fade" id="signup" tabIndex="-1" role="dialog" aria-labelledby="signupLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2 class="modal-title" id="signupLabel">
+                        <b>Demander un partenariat</b>
+                    </h2>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body d-flex flex-column">
+                    <input v-model="nom" class="form-group" type="text" placeholder="Entrez le nom de votre société..."/>
+                    <input v-model="email" class="form-group" type="text" placeholder="Entrez l'email de votre société..."/>
+                    <input v-model="phone" class="form-group" type="tel" placeholder="Entrez le numéro de téléphone de votre société..." />
+                    <input v-model="tva" class="form-group" type="text" placeholder="Entrez votre numéro de TVA..."/>
+                    <input v-model="address" class="form-group" type="text" placeholder="Entrez l'adresse de votre société..."/>
+                    <textarea v-model="description" class="form-group" type="text" placeholder="Décrivez votre société et ses activités..."/>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Envoyer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            nom : "",
+            email : "",
+            phone : "",
+            tva : "",
+            address : "",
+            description : ""
+        }
+    }
+}
+</script>
