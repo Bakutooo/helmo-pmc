e84fd1ba5f6f79c696407b6a6a3fce53dcc072b2
Arnaud RUELL <a.ruell@student.helmo.be>
2018-12-30 00:57:50 +0100

    fixed date selector

--- porte_monnaie_citoyen_web_partner/src/components/modals/ModalAddEvent.vue
+++ porte_monnaie_citoyen_web_partner/src/components/modals/ModalAddEvent.vue
@@ -23,8 +23,25 @@
                                 <div class="form-group">
                                     <input v-model="address" class="form-control" type="text" placeholder="Entrez l'adresse de l'évènement..."/>
                                 </div>
-                                <div class="form-group">
-                                    <input v-model="date" class="form-control" type="datetime-local" />
+                                <div class="form-group d-flex flex-row">
+                                    <!-- <input v-model="date" class="form-control" type="datetime-local" /> -->
+                                    <input v-model="day" class="form-control w-25" type="number" :max="maxDays" min="1" placeholder="Jour">
+                                    <select v-model="month" class="form-control w-50" @change="changeMaxDays()">
+                                        <option disabled value="">Mois</option>
+                                        <option value="01">Janvier</option>
+                                        <option value="02">Février</option>
+                                        <option value="03">Mars</option>
+                                        <option value="04">Avril</option>
+                                        <option value="05">Mai</option>
+                                        <option value="06">Juin</option>
+                                        <option value="07">Juillet</option>
+                                        <option value="08">Août</option>
+                                        <option value="09">Septembre</option>
+                                        <option value="10">Octobre</option>
+                                        <option value="11">Novembre</option>
+                                        <option value="12">Décembre</option>
+                                    </select>
+                                    <input v-model="year" class="form-control w-25" type="number" placeholder="Année">
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" type="file" accept="image/png, image/jpeg" />
@@ -63,8 +80,12 @@ export default {
             name : "",
             description : "",
             address : "",
-            date : "",
-            selectedTown: ""
+            // date : "",
+            selectedTown: "",
+            day : "",
+            month : "",
+            year : "",
+            maxDays : "31"
         }
     },
     computed: {
@@ -83,10 +104,20 @@ export default {
                 name: this.name,
                 description: this.description,
                 address: this.address,
-                date: this.date,
+                date: new Date(this.year, this.month - 1, this.day),
                 partner: this.partner.partner,
                 town: this.selectedTown,
             })
+        },
+        changeMaxDays: function() {
+            let month = this.month;
+            if(month === '1' | month === '3' | month === '5' | month === '7' | month === '8' | month === '10' | month === '12') {
+                this.maxDays = 31;
+            } else if(month === '4' | month === '6' | month === '9' | month === '11') {
+                this.maxDays = 30;
+            } else {
+                this.maxDays = (this.year - 2016) % 4 === 0 ? 29 : 28;
+            }
         }
     }
 }
